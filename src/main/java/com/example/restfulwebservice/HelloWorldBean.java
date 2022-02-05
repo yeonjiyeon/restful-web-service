67cea35b495f782db7736e70b30a204a091fdc51
yeonjiyeon <dalnim95@naver.com>
2022-02-05 14:13:00 +0900

    add HelloWorld

---
+++ src/main/java/com/example/restfulwebservice/HelloWorldBean.java
@@ -0,0 +1,13 @@
+package com.example.restfulwebservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//lombok
+@Data//모든 필드 getter, setter, toString등 만들어줌
+@AllArgsConstructor//모든 아규먼트 추가 생성자
+@NoArgsConstructor//디폰트생성자추가
+public class HelloWorldBean {
+    private String message;
+}
--- src/main/java/com/example/restfulwebservice/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -13,4 +13,9 @@ public class HelloWorldController {
     public String helloWorld(){
         return "Hello World";
     }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");
+    }
 }
