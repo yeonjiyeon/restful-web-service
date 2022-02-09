22a32e97186709dd48f84fd2e83bf983a4d99dfd
yeonjiyeon <dalnim95@naver.com>
2022-02-09 15:35:54 +0900

    add Exception Handling

--- src/main/java/com/example/restfulwebservice/user/UserController.java
+++ src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -1,7 +1,10 @@
 package com.example.restfulwebservice.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -15,17 +18,29 @@ public class UserController {
 
     @GetMapping("/users")
     public List<User> retrieveAllUsers(){
-        return service.findAll();
+        return service.findAll();//바로 반환값을 호출
     }
 
     // GET /users/1 or / users/10 -> String
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id){
-        return service.findOne(id);
+        User user = service.findOne(id);
+        if(user == null){
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+        return user;
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user){
+    public ResponseEntity createUser(@RequestBody User user){
         User savedUser = service.save(user);
+        
+        //사용자한테 요청값 변환해 주기
+        URI location = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
