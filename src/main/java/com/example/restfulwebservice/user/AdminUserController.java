ba81ac5487390c9fc2cd6ed837639efcde1c66a7
yeonjiyeon <dalnim95@naver.com>
2022-02-16 14:08:16 +0900

    add Version

--- src/main/java/com/example/restfulwebservice/user/AdminUserController.java
+++ src/main/java/com/example/restfulwebservice/user/AdminUserController.java
@@ -3,13 +3,13 @@ package com.example.restfulwebservice.user;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
-import org.springframework.http.ResponseEntity;
+
+import org.springframework.beans.BeanUtils;
+
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.validation.Valid;
-import java.net.URI;
+
 import java.util.List;
 
 @RestController
@@ -20,6 +20,7 @@ public class AdminUserController {
     public AdminUserController(UserDaoService service){
         this.service = service;
     }
+
 
     @GetMapping("/users")
     public MappingJacksonValue retrieveAllUsers(){
@@ -35,9 +36,9 @@ public class AdminUserController {
         return mapping;
     }
 
-    // GET /users/1 or / users/10 -> String
-    @GetMapping("/users/{id}")
-    public MappingJacksonValue retrieveUser(@PathVariable int id){
+    //GET /admin/users/1 -> /admin/v1/users/1
+    @GetMapping("v1/users/{id}")
+    public MappingJacksonValue retrieveUserV1(@PathVariable int id){
         User user = service.findOne(id);
         if(user == null){
             throw new UserNotFoundException(String.format("ID[%s] not found", id));
@@ -52,11 +53,31 @@ public class AdminUserController {
         mapping.setFilters(filters);
 
         return mapping;
-
-
     }
 
+    @GetMapping("v2/users/{id}")
+    public MappingJacksonValue retrieveUserV2(@PathVariable int id){
+        User user = service.findOne(id);
+        if(user == null){
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
 
+        //User ->UserV2
+        UserV2 userV2 = new UserV2();
+        BeanUtils.copyProperties(user, userV2);//id, name, joinDate, password, ssn
+        userV2.setGrade("VIP");
+
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id", "name", "joinDate", "grade");
+
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfoV2", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(userV2);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
 
 
 
--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
@@ -13,6 +14,7 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 //@JsonIgnoreProperties(value = {"password", "ssn"})
+@NoArgsConstructor
 @JsonFilter("UserInfo")
 public class User {
     private Integer id;
