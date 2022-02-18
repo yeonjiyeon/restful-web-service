0ad7790736adbed9c2a964930000c805219a3d19
yeonjiyeon <dalnim95@naver.com>
2022-02-18 09:05:18 +0900

    add Version

--- src/main/java/com/example/restfulwebservice/user/AdminUserController.java
+++ src/main/java/com/example/restfulwebservice/user/AdminUserController.java
@@ -37,7 +37,10 @@ public class AdminUserController {
     }
 
     //GET /admin/users/1 -> /admin/v1/users/1
-    @GetMapping("v1/users/{id}")
+    //@GetMapping("v1/users/{id}")
+    //@GetMapping(value = "/users/{id}/", params = "version=1")
+    //@GetMapping(value = "/users/{id}", headers = "X-API-VERSION=1")
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv1+json")
     public MappingJacksonValue retrieveUserV1(@PathVariable int id){
         User user = service.findOne(id);
         if(user == null){
@@ -55,7 +58,10 @@ public class AdminUserController {
         return mapping;
     }
 
-    @GetMapping("v2/users/{id}")
+    //@GetMapping("v2/users/{id}")
+    //@GetMapping(value = "/users/{id}/", params = "version=2")
+   // @GetMapping(value = "/users/{id}", headers = "X-API-VERSION=2")
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv2+json")
     public MappingJacksonValue retrieveUserV2(@PathVariable int id){
         User user = service.findOne(id);
         if(user == null){
