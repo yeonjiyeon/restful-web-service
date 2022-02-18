71cd82bc6727f9ab010ff1f25ed32788c45232a5
yeonjiyeon <dalnim95@naver.com>
2022-02-18 15:13:13 +0900

    add HATEOAS

--- src/main/java/com/example/restfulwebservice/user/UserController.java
+++ src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -1,5 +1,7 @@
 package com.example.restfulwebservice.user;
 
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -7,6 +9,9 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserController {
@@ -24,12 +29,18 @@ public class UserController {
 
     // GET /users/1 or / users/10 -> String
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id){
+    public ResponseEntity<EntityModel<User>> retrieveUser(@PathVariable int id){
         User user = service.findOne(id);
         if(user == null){
             throw new UserNotFoundException(String.format("ID[%s] not found", id));
         }
-        return user;
+
+        //HATEOAS
+        EntityModel entityModel = EntityModel.of(user);
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        //retrieveAllUsers 링크 추가
+        entityModel.add(linkTo.withRel("all-users"));//해당 이름으로 링크가 추가된다
+        return ResponseEntity.ok(entityModel);
     }
 
     @PostMapping("/users")
@@ -52,6 +63,7 @@ public class UserController {
         if (user == null){
             throw new UserNotFoundException(String.format("ID[%s] not found", id));
         }
+
     }
 
     @PutMapping("/users/{id}")
