cc0e3896df3c6aec9c3444e8e398b791f490afb0
yeonjiyeon <dalnim95@naver.com>
2022-02-20 19:33:58 +0900

    add Swagger ui

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -3,6 +3,8 @@ package com.example.restfulwebservice.user;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,17 +18,22 @@ import java.util.Date;
 //@JsonIgnoreProperties(value = {"password", "ssn"})
 @NoArgsConstructor
 //@JsonFilter("UserInfo")
+@ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
 public class User {
     private Integer id;
 
     @Size(min = 2, message = "Name은 2글자 이상 입력해 주세요")
+    @ApiModelProperty(notes = "사용자 이름을 입력해 주세요")
     private String name;
     
     @Past//과거 데이터만 사용
+    @ApiModelProperty(notes = "사용자 등록일을 입력해 주세요")
     private Date joinDate;
 
     //@JsonIgnore
+    @ApiModelProperty(notes = "사용자 패스워드를 입력해 주세요")
     private String password;
    // @JsonIgnore
+   @ApiModelProperty(notes = "사용자 주민번호를 입력해 주세요")
     private String ssn;
 }
