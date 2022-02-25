35da8ceff5bb7180f2159092c2a2591ce15efa8f
yeonjiyeon <dalnim95@naver.com>
2022-02-25 10:29:11 +0900

    add delete, create

--- src/main/java/com/example/restfulwebservice/user/UserJpaController.java
+++ src/main/java/com/example/restfulwebservice/user/UserJpaController.java
@@ -4,11 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -39,5 +39,22 @@ public class UserJpaController {
         entityModel.add(linkTo.withRel("all-users"));
 
         return ResponseEntity.ok(entityModel);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id){
+        userRepository.deleteById(id);
+    }
+    
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user){//RequestBody json타입으로 받기 위해 사용
+        User saveUser = userRepository.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(saveUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+
     }
 }
