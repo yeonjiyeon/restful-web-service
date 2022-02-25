c262dd9a944903d1b194b766f49e4da0865b86ac
yeonjiyeon <dalnim95@naver.com>
2022-02-25 10:42:21 +0900

    add post

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -12,9 +12,11 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -42,4 +44,16 @@ public class User {
    // @JsonIgnore
    @ApiModelProperty(notes = "사용자 주민번호를 입력해 주세요")
     private String ssn;
+
+   @OneToMany(mappedBy = "user")
+   private List<Post> posts;
+
+
+    public User(int id, String name, Date joinDate, String password, String ssn) {
+        this.id = id;
+        this.name = name;
+        this.joinDate = joinDate;
+        this.password = password;
+        this.ssn = ssn;
+    }
 }
